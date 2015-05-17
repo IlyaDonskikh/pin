85b35880dda40d7b060985f154d2c3a371aac29e
doniv <donivrecord@gmail.com>
2015-05-18 01:16:30 +0600

    Refactor db config

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require 'bundler'
 Bundler.require(:default)
 Bundler.require(Sinatra::Base.environment)
 
+require './config/database'
 require_all 'app/**/*.rb'
 require_all 'initializers/*.rb'
 require './app'
--- initializers/redis.rb
+++ initializers/redis.rb
@@ -1,8 +1,4 @@
-require 'yaml'
-config = YAML.load_file('./database.yml')
-config = config[Sinatra::Base.environment.to_s]
-
 REDIS = Redis.new(
-  config: config['host'],
-  port: config['port'],
-  password: config['password'])
+  host: settings.db_host,
+  port: settings.db_port,
+  password: settings.db_password)
