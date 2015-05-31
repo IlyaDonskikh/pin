52f598ad2eea52ecc09d58bc5975ddf3b0412dde
doniv <donivrecord@gmail.com>
2015-05-31 23:09:32 +0600

    Cleanup

--- app/models/redis/base.rb
+++ app/models/redis/base.rb
@@ -1,45 +1,39 @@
 class Redis::Base
   attr_accessor :value, :key, :expire
 
+  class << self
+    def key_with_prefix(key)
+      "#{name.downcase}s:#{key}"
+    end
+
+    def get_value_by(key)
+      REDIS.get key_with_prefix(key)
+    end
+
+    def create(params = {})
+      new(params).save
+    end
+
+    def delete(key)
+      REDIS.del key_with_prefix(key)
+    end
+
+    def incr(key)
+      REDIS.incr key_with_prefix(key)
+    end
+  end
+
   def initialize(attrs = {})
     attrs.each do |name, value|
       send("#{name}=", value)
     end
   end
 
-  def self.prefix
-    "#{name.downcase}s:"
-  end
-
-  def self.combine_prefix(key)
-    prefix + key.to_s
-  end
-
-  def self.get_value_by(key)
-    key = combine_prefix(key)
-
-    REDIS.get(key)
-  end
-
-  def self.create(params = {})
-    new(params).save
-  end
-
-  def self.delete(key)
-    key = combine_prefix(key)
-
-    REDIS.del(key)
-  end
-
-  def self.incr(key)
-    key = combine_prefix(key)
-
-    REDIS.incr(key)
+  def key
+    self.class.key_with_prefix(@key)
   end
 
   def save
-    key = self.class.combine_prefix(self.key)
-
     REDIS.set(key, value, ex: expire)
 
     true
